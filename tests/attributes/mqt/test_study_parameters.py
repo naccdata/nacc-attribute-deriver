54724a9f3b18362ff1c8535a3a883f26c79f8c9d
Emily Cheng <echeng06@uw.edu>
2025-03-18 14:14:41 -0700

    More V2 updates (#10)
    * Fix set issue
    
    * BUILD

---
+++ tests/attributes/mqt/test_study_parameters.py
@@ -0,0 +1,41 @@
+"""Tests deriving MQT study parameters variables."""
+import pytest
+
+from nacc_attribute_deriver.attributes.mqt.study_parameters import StudyParametersAttribute
+from nacc_attribute_deriver.symbol_table import SymbolTable
+
+
+@pytest.fixture(scope='function')
+def attr() -> StudyParametersAttribute:
+    """Create dummy data and return it in an attribute object."""
+    data = {
+        'file': {
+            'info': {
+                'forms': {
+                    'json': {
+                        'formver': "4"
+                    }
+                }
+            }
+        },
+        'subject': {
+            'info': {
+                'study-parameters': {
+                    'uds': {
+                        'versions': [
+                            2
+                        ]
+                    }
+                }
+            }
+        }
+    }
+
+    return StudyParametersAttribute(SymbolTable(data))
+
+
+class TestStudyParametersAttribute:
+
+    def test_create_uds_versions_available(self, attr):
+        """Tests _create_uds_versions_available."""
+        assert attr._create_uds_versions_available() == [2, 4]
