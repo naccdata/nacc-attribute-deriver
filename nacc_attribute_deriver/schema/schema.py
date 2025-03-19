15dffc800b36934720e6f566cc74b3baf0eebb57
Ben Keller <bjkeller@vt.edu>
2025-03-19 16:49:55 +0000

    Change some type names for rule schema
    Change some type names for rule schema
    1. change class and field names to use rule/attribute/assignment terminology
    2. Add model for file input with translation to internal types

--- nacc_attribute_deriver/schema/schema.py
+++ nacc_attribute_deriver/schema/schema.py
@@ -1,4 +1,5 @@
 """Defines the curation schema."""
+
 from typing import List, Optional
 
 from pydantic import BaseModel, field_validator
@@ -6,27 +7,57 @@ from pydantic import BaseModel, field_validator
 from .operation import Operation
 
 
-class DeriveEvent(BaseModel):
-    """Defines a derive event, e.g. where something should go and the event to
-    be applied onto it."""
+class AttributeAssignment(BaseModel):
+    """Defines an assignment of a derived value to an attribute.
+
+    Location is the target attribute operation is how the value is
+    assigned to the attribute
+    """
 
     class Config:
         arbitrary_types_allowed = True
 
-    location: str
+    attribute: str
     operation: Operation
 
-    @field_validator('operation', mode='before')
+    @field_validator("operation", mode="before")
     def generate_operation(cls, value: str) -> Operation:
         return Operation.create(value)
 
 
-class AttributeSchema(BaseModel):
-    """Defines the derive schema for a single attribute, e.g. how something
-    should be derived."""
-    function: str  # Maps to an attribute function
-    events: List[DeriveEvent]  # target DeriveEvents for that function
+class CurationRule(BaseModel):
+    """Defines the curation rules for a single attribute expression.
 
-    # this is more for human readibility, not necessary for any processing
+    Each derived attribute is determined by an assignment operation,
+    and an expression for computing the value.
+    This rule defines the update of several derived attributes by one expression:
+
+    - `function` is the name of a method of an AttributeCollection that
+       implements an attribute expression.
+    - `assignments` is the list of AttributeAssignments that indicate how the
+       value should be assigned to each target attribute.
+    """
+
+    function: str  # Name of the attribute function
+    assignments: List[AttributeAssignment]
+
+    # this is more for human readability, not necessary for any processing
     type: Optional[str] = None
     description: Optional[str] = None
+
+
+class RuleFileModel(BaseModel):
+    """Model for loading serialized rule definitions."""
+
+    function: str
+    location: str
+    operation: str
+    type: str
+    description: str
+
+    @property
+    def assignment(self) -> AttributeAssignment:
+        """Creates an attribute assignment from this rule model."""
+        return AttributeAssignment(
+            attribute=self.location, operation=Operation.create(self.operation)
+        )
