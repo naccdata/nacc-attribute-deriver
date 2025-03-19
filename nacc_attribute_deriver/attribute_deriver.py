a57601f2bc69bb9c5893258265f51d698dfd49f2
Ben Keller <bjkeller@vt.edu>
2025-03-19 22:26:17 +0000

    Refactor attribute deriver to use changes to attribute registry and rule file ingest

--- nacc_attribute_deriver/attribute_deriver.py
+++ nacc_attribute_deriver/attribute_deriver.py
@@ -4,21 +4,24 @@ Gear needs to iterate over all subjects, and for each subject, call this
 AttributeDeriver (deriver.curate(file)) for all (relevant) files in the
 subject. File must correspond to the curation schema.
 """
+
 import csv
+from collections import defaultdict
 from importlib.resources import files
 from pathlib import Path
 from typing import Dict, List, Optional
 
+from pydantic import ValidationError
+
 from .attributes.attribute_collection import AttributeCollectionRegistry
-from .schema.errors import AttributeDeriverException
-from .schema.schema import AttributeSchema, DeriveEvent
+from .schema.errors import AttributeDeriverError
+from .schema.schema import AttributeAssignment, CurationRule, RuleFileModel
 from .symbol_table import SymbolTable
 
 
 class AttributeDeriver:
-
     def __init__(self, date_key: str, rules_file: Optional[Path] = None):
-        """Initiailzer.
+        """Initializer.
 
         Args:
             date_key: Key that determines the order of the forms
@@ -29,9 +32,7 @@ class AttributeDeriver:
         self.__date_key = date_key
         self.__rules = self.__load_rules(rules_file)
 
-    def __load_rules(self,
-                     rules_file: Optional[Path] = None
-                     ) -> List[AttributeSchema]:
+    def __load_rules(self, rules_file: Optional[Path] = None) -> List[CurationRule]:
         """Load rules from the given path. All forms called through curate will
         have these rules applied to them.
 
@@ -41,73 +42,65 @@ class AttributeDeriver:
         # grab default rules from config
         if not rules_file:
             rules_file = files(  # type: ignore
-                "nacc_attribute_deriver").joinpath("config/all_rules.csv")
+                "nacc_attribute_deriver"
+            ).joinpath("config/all_rules.csv")
 
-        # first aggregate all events to their attribute, since some
-        # may have multiple events
-        attributes: Dict[str, List[DeriveEvent]] = {}
-        with rules_file.open('r') as fh:  # type: ignore
+        # aggregate all assignments to their attribute function
+        attributes: Dict[str, List[AttributeAssignment]] = defaultdict(list)
+        with rules_file.open("r") as fh:  # type: ignore
             reader = csv.DictReader(fh)
             if not reader.fieldnames:
-                raise AttributeDeriverException(
-                    "No CSV headers found in derive rules file")
-
-            for exp_header in ['function', 'location', 'operation']:
-                if exp_header not in reader.fieldnames:
-                    raise AttributeDeriverException(
-                        f"Missing expected header: {exp_header}")
+                raise AttributeDeriverError("No CSV headers found in derive rules file")
 
             for row in reader:
-                func = row.pop('function')
-                if func not in attributes:
-                    attributes[func] = []
-                attributes[func].append(DeriveEvent(**row))  # type: ignore
+                try:
+                    rule_schema = RuleFileModel.model_validate(row)
+                except ValidationError as error:
+                    raise AttributeDeriverError(
+                        f"error loading curation rule row: {error}"
+                    )
 
-        # create AttributeSchema for each attribute
-        rules = []
-        for func, events in attributes.items():
+                attributes[rule_schema.function].append(rule_schema.assignment)
+
+        # create rule for each attribute
+        rules: List[CurationRule] = []
+        for attribute_function, assignments in attributes.items():
             rules.append(
-                AttributeSchema(function=f'create_{func}', events=events))
+                CurationRule(
+                    function=f"create_{attribute_function}", assignments=assignments
+                )
+            )
 
         return rules
 
     def curate(self, table: SymbolTable) -> None:
-        """Curate the given file. Assumes has all the FW metadata required to
-        curate the schema. Derived attributes will be added to the same table.
+        """Curate the symbol table with the rules of this deriver.
+
+        Assumes has all the FW metadata required to curate with the schema rules.
+        Derived attributes are added to the same table.
 
         Args:
-            table: Table with file data to curate
+            table: symbol table with subject and file data to curate
         """
-        # make sure date_key is in metadata
         if self.__date_key not in table or not table[self.__date_key]:
-            raise AttributeDeriverException(
-                f"Table does not have specified date key: {self.__date_key}")
+            raise AttributeDeriverError(
+                f"Table does not have specified date key: {self.__date_key}"
+            )
 
         # collect all attributes beforehand so they're easily hashable
-        instance_collections = {}
-        for instance in [
-                c(table) for c in  # type: ignore
-                AttributeCollectionRegistry.collections
-        ]:
-            instance_collections.update({
-                k: {
-                    'func': v,
-                    'instance': instance
-                }
-                for k, v in instance.get_all_hooks().items()
-            })
+        instance_collections = AttributeCollectionRegistry.get_attribute_methods(table)
 
         # derive the variables
-        for attr in self.__rules:
-            hook = instance_collections.get(attr.function, None)
-            if not hook:
-                raise AttributeDeriverException(
-                    f"Unknown attribute function: {attr.function}")
+        for rule in self.__rules:
+            method = instance_collections.get(rule.function, None)
+            if not method:
+                raise AttributeDeriverError(
+                    f"Unknown attribute function: {rule.function}"
+                )
 
-            value = hook['func'](hook['instance'])
+            value = method.apply()
 
-            for event in attr.events:
-                event.operation.evaluate(table,
-                                         value,
-                                         event.location,
-                                         date_key=self.__date_key)
+            for assignment in rule.assignments:
+                assignment.operation.evaluate(
+                    table, value, assignment.attribute, date_key=self.__date_key
+                )
