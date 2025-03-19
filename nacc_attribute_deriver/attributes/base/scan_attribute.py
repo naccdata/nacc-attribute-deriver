c9b1a4790a74eb96f04ff2c84857fbce99d1107e
Ben Keller <bjkeller@vt.edu>
2025-03-19 22:44:28 +0000

    Wrap dictionaries in mapping proxy type

--- nacc_attribute_deriver/attributes/base/scan_attribute.py
+++ nacc_attribute_deriver/attributes/base/scan_attribute.py
@@ -4,7 +4,9 @@ Raw values are split across 7 files - deriving a variable should be isolated
 to a single file as much as possible, otherwise we need to somehow map cross-file
 attributes.
 """
+
 from enum import Enum
+from types import MappingProxyType
 from typing import Any, Optional
 
 from nacc_attribute_deriver.attributes.attribute_collection import AttributeCollection
@@ -12,69 +14,75 @@ from nacc_attribute_deriver.symbol_table import SymbolTable
 
 
 class MRIPrefix(str, Enum):
+    """File name prefixes for SCAN MR-specific files."""
 
-    SCAN_MRI_QC = 'scan_mri_qc.'
-    MRI_SBM = 'mri_sbm.'
+    SCAN_MRI_QC = "scan_mri_qc."
+    MRI_SBM = "mri_sbm."
 
 
 class PETPrefix(str, Enum):
+    """File name prefixes for SCAN PET-specific files."""
 
-    SCAN_PET_QC = 'scan_pet_qc.'
-    AMYLOID_PET_GAAIN = 'amyloid_pet_gaain.'
-    AMYLOID_PET_NPDKA = 'amyloid_pet_npdka.'
-    FDG_PET_NPDKA = 'fdg_pet_npdka.'
-    TAU_PET_NPDKA = 'tau_pet_npdka.'
+    SCAN_PET_QC = "scan_pet_qc."
+    AMYLOID_PET_GAAIN = "amyloid_pet_gaain."
+    AMYLOID_PET_NPDKA = "amyloid_pet_npdka."
+    FDG_PET_NPDKA = "fdg_pet_npdka."
+    TAU_PET_NPDKA = "tau_pet_npdka."
 
     @classmethod
     def analysis_files(cls):
-        """Returns all analysis PET files."""
+        """Returns the names of all analysis PET files."""
         return [
-            cls.AMYLOID_PET_GAAIN, cls.AMYLOID_PET_NPDKA, cls.FDG_PET_NPDKA,
-            cls.TAU_PET_NPDKA
+            cls.AMYLOID_PET_GAAIN,
+            cls.AMYLOID_PET_NPDKA,
+            cls.FDG_PET_NPDKA,
+            cls.TAU_PET_NPDKA,
         ]
 
 
 class SCANAttribute(AttributeCollection):
+    TRACER_MAPPING = MappingProxyType(
+        {
+            1: "fdg",
+            2: "pib",
+            3: "florbetapir",
+            4: "florbetaben",
+            5: "nav4694",
+            6: "flortaucipir",
+            7: "MK6240",
+            8: "pi2620",
+            9: "gtp1",
+            99: "unknown",
+        }
+    )
 
-    TRACER_MAPPING = {
-        1: "fdg",
-        2: "pib",
-        3: "florbetapir",
-        4: "florbetaben",
-        5: "nav4694",
-        6: "flortaucipir",
-        7: "MK6240",
-        8: "pi2620",
-        9: "gtp1",
-        99: "unknown",
-    }
+    TRACER_SCAN_TYPE_MAPPING = MappingProxyType(
+        {
+            1: "fdg",
+            2: "amyloid",
+            3: "amyloid",
+            4: "amyloid",
+            5: "amyloid",
+            6: "tau",
+            7: "tau",
+            8: "tau",
+            9: "tau",
+        }
+    )
 
-    TRACER_SCAN_TYPE_MAPPING = {
-        1: "fdg",
-        2: "amyloid",
-        3: "amyloid",
-        4: "amyloid",
-        5: "amyloid",
-        6: "tau",
-        7: "tau",
-        8: "tau",
-        9: "tau",
-    }
-
-    def __init__(self,
-                 table: SymbolTable,
-                 form_prefix: str = 'file.info.raw.',
-                 mri_prefix: str = 'file.info.raw.mri.scan.',
-                 pet_prefix: str = 'file.info.raw.pet.scan.') -> None:
+    def __init__(
+        self,
+        table: SymbolTable,
+        form_prefix: str = "file.info.raw.",
+        mri_prefix: str = "file.info.raw.mri.scan.",
+        pet_prefix: str = "file.info.raw.pet.scan.",
+    ) -> None:
         """Override initializer to set prefix to SCAN-specific data."""
         super().__init__(table, form_prefix)
         self.__mri_prefix = mri_prefix
         self.__pet_prefix = pet_prefix
 
-    def get_mri_value(self,
-                      key: str,
-                      subprefix: MRIPrefix,
-                      default: Any = None) -> Any:
+    def get_mri_value(self, key: str, subprefix: MRIPrefix, default: Any = None) -> Any:
         """Get MRI-specific value.
 
         Args:
@@ -82,14 +90,11 @@ class SCANAttribute(AttributeCollection):
             subprefix: The MRI-specific sub-prefix
             default: Default value to return if key is not found
         """
-        return self.get_value(key,
-                              default,
-                              prefix=f'{self.__mri_prefix}{subprefix.value}')
+        return self.get_value(
+            key, default, prefix=f"{self.__mri_prefix}{subprefix.value}"
+        )
 
-    def get_pet_value(self,
-                      key: str,
-                      subprefix: PETPrefix,
-                      default: Any = None) -> Any:
+    def get_pet_value(self, key: str, subprefix: PETPrefix, default: Any = None) -> Any:
         """Get PET-specific value.
 
         Args:
@@ -97,9 +102,9 @@ class SCANAttribute(AttributeCollection):
             subprefix: The PET-specific sub-prefix
             default: Default value to return if key is not found
         """
-        return self.get_value(key,
-                              default,
-                              prefix=f'{self.__pet_prefix}{subprefix.value}')
+        return self.get_value(
+            key, default, prefix=f"{self.__pet_prefix}{subprefix.value}"
+        )
 
     # get functions for common values
     def get_tracer(self, field: str, subprefix: PETPrefix) -> Optional[str]:
