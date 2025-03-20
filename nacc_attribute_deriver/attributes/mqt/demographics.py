a3ab6201e53e8097c430d469b5ce87df3ecdcf78
Ben Keller <bjkeller@vt.edu>
2025-03-20 23:48:10 +0000

    Split demographics b/w uds and derived attributes

--- nacc_attribute_deriver/attributes/mqt/demographics.py
+++ nacc_attribute_deriver/attributes/mqt/demographics.py
@@ -3,45 +3,21 @@
 Assumes NACC-derived variables are already set
 """
 
+from types import MappingProxyType
 from typing import Optional
 
-from nacc_attribute_deriver.attributes.base.base_attribute import FormAttribute
+from nacc_attribute_deriver.attributes.base.base_attribute import DerivedAttribute
+from nacc_attribute_deriver.attributes.nacc.modules.uds.uds_attribute import (
+    UDSAttribute,
+)
 
 
-class DemographicsAttribute(FormAttribute):
+class DemographicsAttribute(UDSAttribute):
     """Class to collect demographic attributes."""
 
-    SEX_MAPPING = {1: "Male", 2: "Female", 8: "Prefer not to answer", 9: "Don't know"}
-
-    RACE_MAPPING = {
-        1: "White",
-        2: "Black or African American",
-        3: "American Indian or Alaska Native",
-        4: "Native Hawaiian or Pacific Islander",
-        5: "Asian",
-        6: "Multiracial",
-        7: "Middle Eastern or Northern African",
-        8: "Hispanic or Latino",  # TODO for UDSv4?
-        99: "Unknown or ambiguous",
-    }
-
-    PRIMARY_LANGUAGE_MAPPING = {
-        1: "English",
-        2: "Spanish",
-        3: "Mandarin",
-        4: "Cantonese",
-        5: "Russian",
-        6: "Japanese",
-        8: "Other primary language (specify)",
-        9: "Unknown",
-    }
-
-    VITAL_STATUS_MAPPINGS = {0: "Not deceased/unknown", 1: "Deceased"}
-
-    def _create_uds_age(self) -> int:
-        """UDS age at form date, mapped from NACCAGE."""
-        result = self.assert_required(["naccage"], prefix="file.info.derived.")
-        return result["naccage"]
+    SEX_MAPPING = MappingProxyType(
+        {1: "Male", 2: "Female", 8: "Prefer not to answer", 9: "Don't know"}
+    )
 
     def _create_uds_sex(self) -> Optional[str]:
         """UDS sex."""
@@ -54,12 +30,18 @@ class DemographicsAttribute(FormAttribute):
         except TypeError:
             return None
 
-        return None
-
-    def _create_uds_race(self) -> str:
-        """UDS race."""
-        result = self.assert_required(["naccnihr"], prefix="file.info.derived.")
-        return self.RACE_MAPPING.get(result["naccnihr"], "Unknown or ambiguous")
+    PRIMARY_LANGUAGE_MAPPING = MappingProxyType(
+        {
+            1: "English",
+            2: "Spanish",
+            3: "Mandarin",
+            4: "Cantonese",
+            5: "Russian",
+            6: "Japanese",
+            8: "Other primary language (specify)",
+            9: "Unknown",
+        }
+    )
 
     def _create_uds_primary_language(self) -> str:
         """UDS primary language."""
@@ -70,12 +52,40 @@ class DemographicsAttribute(FormAttribute):
         """UDS education level."""
         return self.get_value("educ", None)
 
+
+class DerivedDemographicsAttribute(DerivedAttribute):
+    def _create_uds_age(self) -> int:
+        """UDS age at form date, mapped from NACCAGE."""
+        result = self.assert_required(["naccage"])
+        return result["naccage"]
+
+    RACE_MAPPING = MappingProxyType(
+        {
+            1: "White",
+            2: "Black or African American",
+            3: "American Indian or Alaska Native",
+            4: "Native Hawaiian or Pacific Islander",
+            5: "Asian",
+            6: "Multiracial",
+            7: "Middle Eastern or Northern African",
+            8: "Hispanic or Latino",  # TODO for UDSv4?
+            99: "Unknown or ambiguous",
+        }
+    )
+
+    def _create_uds_race(self) -> str:
+        """UDS race."""
+        result = self.assert_required(["naccnihr"])
+        return self.RACE_MAPPING.get(result["naccnihr"], "Unknown or ambiguous")
+
     def _create_age_at_death(self) -> int:
         """Age at death, mapped from NACCDAGE."""
-        result = self.assert_required(["naccdage"], prefix="file.info.derived.")
+        result = self.assert_required(["naccdage"])
         return result["naccdage"]
+
+    VITAL_STATUS_MAPPINGS = MappingProxyType({0: "Not deceased/unknown", 1: "Deceased"})
 
     def _create_vital_status(self) -> str:
         """Creates subject.info.demographics.uds.vital-status.latest."""
-        result = self.assert_required(["naccdied"], prefix="file.info.derived.")
+        result = self.assert_required(["naccdied"])
         return self.VITAL_STATUS_MAPPINGS.get(result["naccdied"], "Unknown")
