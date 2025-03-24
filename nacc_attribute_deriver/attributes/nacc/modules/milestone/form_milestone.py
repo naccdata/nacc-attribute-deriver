d30818b03afdc46cde064d18a124ade926098223
Ben Keller <bjkeller@vt.edu>
2025-03-24 18:46:59 +0000

    Add milestone rules

---
+++ nacc_attribute_deriver/attributes/nacc/modules/milestone/form_milestone.py
@@ -0,0 +1,25 @@
+from datetime import date
+from typing import Optional
+
+from nacc_attribute_deriver.attributes.attribute_collection import AttributeCollection
+from nacc_attribute_deriver.attributes.base.base_attribute import FormAttribute
+from nacc_attribute_deriver.symbol_table import SymbolTable
+from nacc_attribute_deriver.utils.date import create_death_date
+
+
+class MilestoneAttributeCollection(AttributeCollection):
+    def __init__(self, table: SymbolTable):
+        self.__milestone = FormAttribute(table)
+
+    def _create_milestone_death_date(self) -> Optional[date]:
+        if not self.is_int_value(self.__milestone.get_value("deceased"), 1):
+            return None
+
+        year = self.__milestone.get_value("deathyr")
+        month = self.__milestone.get_value("deathmo")  # can be 99
+        day = self.__milestone.get_value("deathdy")  # can be 99
+
+        return create_death_date(year=year, month=month, day=day)
+
+    def _create_milestone_deceased(self) -> Optional[int]:
+        return self.__milestone.get_value("deceased")
