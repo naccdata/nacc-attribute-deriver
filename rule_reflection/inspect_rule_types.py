b913059b4c399723d736700ea8e8b92ed78080df
Ben Keller <bjkeller@vt.edu>
2025-05-27 20:30:07 +0000

    Refactor rule reflection

--- rule_reflection/inspect_rule_types.py
+++ rule_reflection/inspect_rule_types.py
@@ -1,9 +1,14 @@
-from csv import DictReader
+"""Computes the types of derived attributes."""
+
+import argparse
+import logging
+import sys
+from csv import DictReader, DictWriter
 from importlib import resources
 from inspect import signature
-from typing import Union, get_args, get_origin
+from typing import List, Union, get_args, get_origin
 
-from pydantic import ValidationError
+from pydantic import BaseModel, ValidationError
 
 from nacc_attribute_deriver import config
 from nacc_attribute_deriver.attributes.attribute_collection import (
@@ -11,8 +16,17 @@ from nacc_attribute_deriver.attributes.attribute_collection import (
 )
 from nacc_attribute_deriver.schema.schema import RuleFileModel
 
+log = logging.getLogger(__name__)
+
 
 def type_string(type_object: type) -> str:
+    """Returns the type as a string.
+
+    Args:
+      type_object: the type
+    Returns:
+      the type as a string
+    """
     if type_object is type(None):
         return "None"
 
@@ -35,6 +49,8 @@ def type_string(type_object: type) -> str:
 
 
 class TypeWrapper:
+    """Wrapper for a type."""
+
     def __init__(self, wrapped_type: type) -> None:
         self.wrapped_type = wrapped_type
 
@@ -42,40 +58,79 @@ class TypeWrapper:
         return type_string(self.wrapped_type)
 
     def is_optional(self) -> bool:
+        """Indicates whether this type is optional."""
+
         return get_origin(self.wrapped_type) is Union and type(None) in get_args(
             self.wrapped_type
         )
 
 
+class RuleType(BaseModel):
+    attribute: str
+    attribute_type: str
+    operation: str
+    expression: str
+    expression_type: str
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="list source attributes in data model")
+    parser.add_argument("--output", "-o", help="path to save output file")
+    return parser.parse_args()
+
+
 def main():
+    args = parse_arguments()
+    output_file = args.output if args.output else "rule-types.csv"
+
     instance_collections = AttributeCollectionRegistry.get_attribute_methods()
     rules_file = resources.files(config).joinpath("curation_rules.csv")
     with rules_file.open("r") as file_stream:
         reader = DictReader(file_stream)
         if not reader.fieldnames:
-            print("whoa. no fieldnames")
+            log.error("Expecting fieldnames in rules file.")
+            sys.exit(1)
 
-        print("attribute,attribute_type,operation,expression,expression_type")
+        rule_types: List[RuleType] = []
         for row in reader:
             try:
                 rule_schema = RuleFileModel.model_validate(row)
             except ValidationError as error:
-                print(error)
+                log.error("Rule validation error: %s", str(error))
                 continue
 
             # location, operation, type
             expression = instance_collections.get(f"create_{rule_schema.function}")
             if not expression:
                 continue
-            sig = signature(expression.function)
-            return_type = sig.return_annotation
-            expression_type = TypeWrapper(return_type)
-            operation = rule_schema.assignment.operation
-            attribute_type = TypeWrapper(operation.attribute_type(return_type))
 
-            print(
-                f"{rule_schema.location},{attribute_type},{rule_schema.operation},{rule_schema.function},{expression_type}"
+            return_type = signature(expression.function).return_annotation
+            rule_types.append(
+                RuleType(
+                    attribute=rule_schema.location,
+                    attribute_type=str(
+                        TypeWrapper(
+                            rule_schema.assignment.operation.attribute_type(return_type)
+                        )
+                    ),
+                    operation=rule_schema.operation,
+                    expression=rule_schema.function,
+                    expression_type=str(TypeWrapper(return_type)),
+                )
             )
+
+        with open(output_file, "w", encoding="utf-8") as out_file:
+            fieldnames = [
+                "attribute",
+                "attribute_type",
+                "operation",
+                "expression",
+                "expression_type",
+            ]
+            writer = DictWriter(out_file, fieldnames=fieldnames)
+            writer.writeheader()
+            for rule_type in rule_types:
+                writer.writerow(rule_type.model_dump())
 
 
 if __name__ == "__main__":
