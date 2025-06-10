dbedd7eece8520d98079588cfb4f9322b61bd471
Ben Keller <bjkeller@vt.edu>
2025-06-10 16:56:56 +0000

    Update nacc scan.py

--- nacc_attribute_deriver/attributes/nacc/imaging/scan.py
+++ nacc_attribute_deriver/attributes/nacc/imaging/scan.py
@@ -2,9 +2,9 @@
 
 from nacc_attribute_deriver.attributes.attribute_collection import AttributeCollection
 from nacc_attribute_deriver.attributes.base.scan_namespace import (
+    SCAN_REQUIRED_FIELDS,
     SCANNamespace,
 )
-from nacc_attribute_deriver.schema.errors import MissingRequiredError
 from nacc_attribute_deriver.symbol_table import SymbolTable
 from nacc_attribute_deriver.utils.date import datetime_from_form_date
 from nacc_attribute_deriver.utils.scope import (
@@ -13,35 +13,41 @@ from nacc_attribute_deriver.utils.scope import (
 )
 
 
-class NACCSCANAttributeCollection(AttributeCollection):
+class SCANMRIAttributeCollection(AttributeCollection):
     """Class to collect NACC SCAN attributes needed to derive MQT."""
 
     def __init__(self, table: SymbolTable):
-        self.__scan = SCANNamespace(table)
+        mri_required = SCAN_REQUIRED_FIELDS.get(SCANMRIScope.MRI_QC)
+        self.__scan_mri = SCANNamespace(
+            table=table,
+            required=frozenset(mri_required),  # type: ignore
+        )
 
     def _create_scan_mri_dates(self) -> str:
         """Gets the date of the MRI scan - temporary derived variable."""
-        scandate = self.__scan.scope(name=SCANMRIScope.MRI_QC).get_value("study_date")
+        scandate = self.__scan_mri.get_value("study_date")
+        assert scandate is not None
 
         scandate = datetime_from_form_date(scandate)
-
-        if not scandate:
-            raise MissingRequiredError(
-                message="study_date (from SCAN_MRI_QC) required",
-                field="study_date",
-            )
 
         return str(scandate.date())
 
+
+class SCANPETAttributeCollection(AttributeCollection):
+    """Class to collect NACC SCAN attributes needed to derive MQT."""
+
+    def __init__(self, table: SymbolTable):
+        pet_required = SCAN_REQUIRED_FIELDS.get(SCANPETScope.PET_QC)
+        self.__scan_pet = SCANNamespace(
+            table=table,
+            required=frozenset(pet_required),  # type: ignore
+        )
+
     def _create_scan_pet_dates(self) -> str:
         """Gets the date of the PET scan - temporary derived variable"""
-        scandate = self.__scan.scope(name=SCANPETScope.PET_QC).get_value("scan_date")
+        scandate = self.__scan_pet.get_value("scan_date")
+        assert scandate is not None
 
         scandate = datetime_from_form_date(scandate)
-        if not scandate:
-            raise MissingRequiredError(
-                message="scan_date (from SCAN_PET_QC) required",
-                field="scan_date",
-            )
 
         return str(scandate.date())
