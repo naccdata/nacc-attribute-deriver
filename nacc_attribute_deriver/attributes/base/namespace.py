ad047ee970cd3d4002aac31f75a985f9d13a63e5
Ben Keller <bjkeller@vt.edu>
2025-06-11 16:24:43 +0000

    Change namespace to include required fields checked at initialization

--- nacc_attribute_deriver/attributes/base/namespace.py
+++ nacc_attribute_deriver/attributes/base/namespace.py
@@ -2,11 +2,18 @@
 
 import datetime
 import logging
-from typing import Any, Generic, List, Optional, TypeVar
+from typing import (
+    Any,
+    Generic,
+    Iterable,
+    List,
+    Optional,
+    TypeVar,
+)
 
 from pydantic import BaseModel, ConfigDict, field_serializer
 
-from nacc_attribute_deriver.schema.errors import MissingRequiredError
+from nacc_attribute_deriver.schema.errors import InvalidFieldError, MissingRequiredError
 from nacc_attribute_deriver.symbol_table import SymbolTable
 from nacc_attribute_deriver.utils.date import datetime_from_form_date
 
@@ -35,8 +42,10 @@ class BaseNamespace:
 
     def __init__(
         self,
+        *,
         table: SymbolTable,
         attribute_prefix: str,
+        required: frozenset[str] = frozenset(),
         date_attribute: Optional[str] = None,
     ):
         """Initializes an attribute wrapper, setting the prefix for accessing
@@ -45,61 +54,67 @@ class BaseNamespace:
         Args:
           table: the symbol table
           attribute_prefix: the prefix used to access values
+
+        Raises:
+          MissingRequiredError if any required attributes are missing
         """
         self.__table = table
         self.__prefix = (
             attribute_prefix if attribute_prefix[-1] == "." else f"{attribute_prefix}."
         )
         self.__date_attribute = date_attribute
+        self.__required = (
+            required.union([date_attribute]) if date_attribute else required
+        )
 
-    def __contains__(self, key: str) -> bool:
-        return f"{self.__prefix}{key}" in self.__table
+        self.__check_required()
+
+    def __check_required(self) -> None:
+        """Check that required attributes have a value in the table.
+
+        Raises:
+          MissingRequiredError if any required attributes are missing
+        """
+        missing: List[str] = []
+        for attribute in self.__required:
+            value = self.get_value(attribute)
+            if value is not None:
+                continue
+
+            missing.append(self.__symbol(attribute))
+
+        if missing:
+            raise MissingRequiredError(missing)
 
     @property
     def prefix(self) -> str:
         """Returns the attribute prefix for this object."""
         return self.__prefix
 
-    def get_value(self, attribute: str, default: Optional[Any] = None) -> Any:
-        """Returns the value of the attribute key in the table.
+    def __symbol(self, attribute: str) -> str:
+        """Returns the symbol table path for the attribute."""
+        return f"{self.prefix}{attribute}"
+
+    def __contains__(self, attribute: str) -> bool:
+        """Indicates whether the attribute occurs in the table."""
+        return self.__symbol(attribute) in self.__table
+
+    def is_required(self, attribute: str) -> bool:
+        """Indicates whether the attribute is required in this namespace.
 
         Args:
-          key: the attribute name
-          default: the default value
+          attribute: the attribute name
         Returns:
-          the value for the attribute in the table
+          True if the attribute is required. False, otherwise.
         """
-        return self.__table.get(f"{self.prefix}{attribute}", default)
+        return attribute in self.__required
 
-    def get_date(self) -> Optional[datetime.date]:
-        """Returns the value for the date-attribute if defined.
+    @property
+    def required_fields(self) -> Iterable[str]:
+        """Returns the required fields for this namespace."""
+        return self.__required
 
-        Returns:
-          the value of the date-attribute if defined. None, otherwise.
-        """
-        if self.__date_attribute is None:
-            return None
-
-        file_date = datetime_from_form_date(self.get_value(self.__date_attribute))
-        if not file_date:
-            return None
-
-        return file_date.date()
-
-    def get_dated_value(
-        self, attribute: str, default: Optional[Any] = None
-    ) -> DateTaggedValue[Any]:
-        """Grab value from the table using the key and prefix, if provided.
-
-        Args:
-            key: Key to grab value for
-            default: Default value to return if key is not found
-        """
-        return DateTaggedValue(
-            value=self.get_value(attribute=attribute, default=default),
-            date=self.get_date(),
-        )
-
+    # TODO: remove this after all attribute collections transitioned
     def assert_required(self, required: List[str]) -> bool:
         """Asserts that the given fields in required are in the table for the
         source.
@@ -118,17 +133,142 @@ class BaseNamespace:
 
         return True
 
+    def get_value(self, attribute: str, default: Optional[Any] = None) -> Optional[Any]:
+        """Returns the value of the attribute key in the table.
+
+        Args:
+          key: the attribute name
+          default: the default value
+        Returns:
+          the value for the attribute in the table
+        """
+        return self.__table.get(self.__symbol(attribute), default)  # type: ignore
+
+    def get_date(self) -> Optional[datetime.date]:
+        """Returns the value for the date-attribute if defined.
+
+        Returns:
+          the value of the date-attribute if defined. None, otherwise.
+        """
+        if self.__date_attribute is None:
+            return None
+
+        file_date = datetime_from_form_date(self.get_value(self.__date_attribute))
+        if not file_date:
+            return None
+
+        return file_date.date()
+
+    def create_dated_value(
+        self,
+        attribute: str,
+        date: Optional[datetime.date],
+        default: Optional[Any] = None,
+    ) -> Optional[DateTaggedValue[Any]]:
+        """Create a DateTaggedValue for the value of the attribute from this
+        namespace.
+
+        Args:
+          attribute: the attribute
+          date: the date to tag the value with
+          default: value to use if the attribute has no value in this namespace.
+        Returns:
+          value of the attribute tagged by the date. None if there is no value.
+        """
+        attribute_value = self.get_value(attribute=attribute, default=default)
+        if attribute_value is None:
+            return None
+
+        return DateTaggedValue(value=attribute_value, date=date)
+
+    def get_dated_value(
+        self, attribute: str, default: Optional[Any] = None
+    ) -> Optional[DateTaggedValue[Any]]:
+        """Grab value from the table using the key and prefix, if provided.
+
+        Args:
+            key: Key to grab value for
+            default: Default value to return if key is not found
+        """
+        return self.create_dated_value(attribute, self.get_date(), default)
+
+    def get_int_value(self, attribute: str) -> Optional[int]:
+        """Gets the value for an integer attribute.
+
+        Args:
+          attribute: the attribute
+        Returns:
+          the integer value of attribute if exists. None, otherwise.
+        Raises:
+          TypeError if the value of the attribute is not an integer
+        """
+        attribute_value = self.get_value(attribute)
+        if attribute_value is None:
+            return None
+
+        try:
+            return int(attribute_value)
+        except ValueError as error:
+            raise InvalidFieldError(f"{attribute} expected to be an integer") from error
+
+    def get_float(self, attribute: str) -> float:
+        """Gets the float value of a required attribute.
+
+        Args:
+          attribute: attribute name
+        Returns:
+          the float value of the attribute
+        Raises:
+          InvalidFieldError if the attribute does not have a float value
+        """
+        assert self.is_required(attribute)
+        attribute_value = self.get_value(attribute)
+        assert attribute_value is not None
+
+        try:
+            return float(attribute_value)
+        except (ValueError, TypeError) as error:
+            raise InvalidFieldError(
+                f"expected {attribute} to have float value, got {attribute_value}"
+            ) from error
+
+    def get_count(self, attribute: str) -> int:
+        """Returns the length of the value of the required attribute.
+
+        Args:
+          attribute: the attribute
+        Returns:
+          the length of the attribute value. None if there is no attribute
+        """
+        assert self.is_required(attribute)
+        attribute_value = self.get_value(attribute)
+        assert attribute_value is not None
+
+        try:
+            return len(attribute_value)
+        except TypeError as error:
+            raise InvalidFieldError(
+                f"expected {attribute} to have a length, got value {attribute_value}"
+            ) from error
+
 
 class FormNamespace(BaseNamespace):
     """Base class for attributes over file.info.forms."""
 
     def __init__(
         self,
+        *,
         table: SymbolTable,
         attribute_prefix: str = "file.info.forms.json.",
-        date_attribute="visitdate",
+        required: frozenset[str] = frozenset(),
+        date_attribute: str = "visitdate",
     ) -> None:
-        super().__init__(table, attribute_prefix, date_attribute)
+        super().__init__(
+            table=table,
+            attribute_prefix=attribute_prefix,
+            required=required,
+            date_attribute=date_attribute,
+        )
 
 
 class RawNamespace(BaseNamespace):
@@ -138,9 +278,15 @@ class RawNamespace(BaseNamespace):
         self,
         table: SymbolTable,
         attribute_prefix: str = "file.info.raw.",
+        required: frozenset[str] = frozenset(),
         date_attribute: Optional[str] = None,
     ) -> None:
-        super().__init__(table, attribute_prefix, date_attribute)
+        super().__init__(
+            table=table,
+            attribute_prefix=attribute_prefix,
+            required=required,
+            date_attribute=date_attribute,
+        )
 
 
 class DerivedNamespace(BaseNamespace):
@@ -148,31 +294,52 @@ class DerivedNamespace(BaseNamespace):
 
     def __init__(
         self,
+        *,
         table: SymbolTable,
-        attribute_prefix="file.info.derived.",
+        attribute_prefix: str = "file.info.derived.",
+        required: frozenset[str] = frozenset(),
         date_attribute: Optional[str] = None,
     ) -> None:
-        super().__init__(table, attribute_prefix, date_attribute)
+        super().__init__(
+            table=table,
+            attribute_prefix=attribute_prefix,
+            required=required,
+            date_attribute=date_attribute,
+        )
 
 
 class SubjectInfoNamespace(BaseNamespace):
     def __init__(
         self,
+        *,
         table: SymbolTable,
         attribute_prefix: str = "subject.info.",
+        required: frozenset[str] = frozenset(),
         date_attribute: Optional[str] = None,
     ):
-        super().__init__(table, attribute_prefix, date_attribute)
+        super().__init__(
+            table=table,
+            attribute_prefix=attribute_prefix,
+            required=required,
+            date_attribute=date_attribute,
+        )
 
 
 class SubjectDerivedNamespace(BaseNamespace):
     def __init__(
         self,
+        *,
         table: SymbolTable,
         attribute_prefix: str = "subject.info.derived.",
+        required: frozenset[str] = frozenset(),
         date_attribute: Optional[str] = None,
     ) -> None:
-        super().__init__(table, attribute_prefix, date_attribute)
+        super().__init__(
+            table=table,
+            attribute_prefix=attribute_prefix,
+            required=required,
+            date_attribute=date_attribute,
+        )
 
     def get_cross_sectional_value(
         self, attribute: str, default: Optional[Any] = None
