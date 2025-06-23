1492b9127a712f9d6e9186e0781de3b97ed96d81
Ben Keller <bjkeller@vt.edu>
2025-06-23 16:01:28 +0000

    Add types to prefix fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,30 +9,30 @@ def set_attribute(table: SymbolTable, prefix: str, attribute: str, value: Any) -
 
 
 @fixture
-def file_prefix():
+def file_prefix() -> str:
     return "file.info."
 
 
 @fixture
-def form_prefix():
+def form_prefix() -> str:
     return "file.info.forms.json."
 
 
 @fixture
-def raw_prefix():
+def raw_prefix() -> str:
     return "file.info.raw."
 
 
 @fixture
-def derived_prefix():
+def derived_prefix() -> str:
     return "file.info.derived."
 
 
 @fixture
-def subject_prefix():
+def subject_prefix() -> str:
     return "subject.info."
 
 
 @fixture
-def subject_derived_prefix():
+def subject_derived_prefix() -> str:
     return "subject.info.derived."
