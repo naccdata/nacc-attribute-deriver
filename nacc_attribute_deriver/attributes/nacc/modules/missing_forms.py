9ea0f91c4cea9c3d71e89ff3aeeed3d06e08eea4
Emily Cheng <echeng06@uw.edu>
2025-07-16 09:29:32 -0700

    Handle NIAGADS variables

--- nacc_attribute_deriver/attributes/nacc/modules/missing_forms.py
+++ nacc_attribute_deriver/attributes/nacc/modules/missing_forms.py
@@ -16,24 +16,53 @@ class MissingFormAttributes(AttributeCollection):
     def __init__(self, table: SymbolTable):
         self.__derived = SubjectDerivedNamespace(table=table)
 
-    def _create_uds_naccnrdy(self) -> Optional[int]:
-        """Handles NACCNRDY (from Milestones)."""
-        if not self.__derived.get_cross_sectional_value("naccnrdy", int):
-            return 88
+    def handle_missing(self, attribute: str, default: int) -> Optional[int]:
+        """Handle missing values."""
+        if not self.__derived.get_cross_sectional_value(attribute, int):
+            return default
 
         return None
+
+    def _create_uds_naccnrdy(self) -> Optional[int]:
+        """Handles NACCNRDY (from Milestones)."""
+        return self.handle_missing("naccnrdy", 88)
 
     def _create_uds_naccnrmo(self) -> Optional[int]:
         """Handles NACCNRMO (from Milestones)."""
-        if not self.__derived.get_cross_sectional_value("naccnrmo", int):
-            return 88
-
-        return None
+        return self.handle_missing("naccnrmo", 88)
 
     def _create_uds_naccnryr(self) -> Optional[int]:
         """Handles NACCNRYR (from Milestones)."""
-        if not self.__derived.get_cross_sectional_value("naccnyr", int):
-            return 8888
+        return self.handle_missing("naccnyr", 8888)
 
-        return None
+    def _create_uds_ngdsgwas(self) -> int:
+        """Handles NGDSGWAS (from NIAGADS)."""
+        return self.handle_missing("ngdsgwas", 0)
 
+    def _create_uds_ngdsexom(self) -> int:
+        """Handles NGDSEXOM (from NIAGADS)."""
+        return self.handle_missing("ngdsexom", 0)
+
+    def _create_uds_ngdswgs(self) -> int:
+        """Handles NGDSWGS (from NIAGADS)."""
+        return self.handle_missing("ngdswgs", 0)
+
+    def _create_uds_ngdswes(self) -> int:
+        """Handles NGDSWES (from NIAGADS)."""
+        return self.handle_missing("ngdswes", 0)
+
+    def _create_uds_ngdsgwac(self) -> str:
+        """Handles NGDSGWAC (from NIAGADS)"""
+        return self.handle_missing("ngdsgwac", 88)
+
+    def _create_uds_ngdsexac(self) -> str:
+        """Handles NGDSEXAC (from NIAGADS)."""
+        return self.handle_missing("ngdsexac", 88)
+
+    def _create_uds_ngdswgac(self) -> str:
+        """Handles NGDSWGAC (from NIAGADS)."""
+        return self.handle_missing("ngdswgac", 88)
+
+    def _create_uds_ngdsweac(self) -> str:
+        """Handles NGDSWEAC (from NIAGADS)."""
+        return self.handle_missing("ngdsweac", 88)
