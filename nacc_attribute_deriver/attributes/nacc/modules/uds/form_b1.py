1bcb81d93c2ab47ee98c737bf8176d8466fe72ac
Emily Cheng <echeng06@uw.edu>
2025-10-16 16:11:49 -0700

    Add B1 derived variables

--- nacc_attribute_deriver/attributes/nacc/modules/uds/form_b1.py
+++ nacc_attribute_deriver/attributes/nacc/modules/uds/form_b1.py
@@ -8,6 +8,11 @@ from typing import Optional
 from nacc_attribute_deriver.attributes.collection.uds_attribute import (
     UDSAttributeCollection,
 )
+from nacc_attribute_deriver.schema.constants import (
+    INFORMED_MISSINGNESS,
+    NOT_ASSESSED_FLOAT,
+)
+from nacc_attribute_deriver.schema.errors import AttributeDeriverError
 
 
 class UDSFormB1Attribute(UDSAttributeCollection):
@@ -24,7 +29,7 @@ class UDSFormB1Attribute(UDSAttributeCollection):
         """
         height = self.uds.get_value("height", float)
 
-        if height is None or height == 99 or (self.formver == 3 and height == 88):
+        if height is None or height == 99 or (self.formver >= 3 and height >= 88):
             return None
 
         heigdec = self.uds.get_value("heigdec", float)
@@ -47,7 +52,7 @@ class UDSFormB1Attribute(UDSAttributeCollection):
         # submitted on initial visit, -4/None otherwise
         if not self.submitted:
             if self.uds.is_initial():
-                return 888.8
+                return INFORMED_MISSINGNESS
             return None
 
         height = self.get_height()
@@ -59,4 +64,96 @@ class UDSFormB1Attribute(UDSAttributeCollection):
             # + 0.0001 so we ensure exact halves round up, not down
             return round(naccbmi + 0.0001, 1)
 
-        return 888.8
+        return NOT_ASSESSED_FLOAT
+
+    def _compute_average(self, field1: str, field2: str) -> Optional[float]:
+        """Compute the average for the two fields (V4 only)."""
+        if self.formver < 4:
+            return INFORMED_MISSINGNESS
+
+        if not self.submitted:
+            if self.uds.is_initial():
+                return INFORMED_MISSINGNESS
+            return None
+
+        value1 = self.uds.get_value(field1, float)
+        value2 = self.uds.get_value(field2, float)
+
+        if any(x is None or x == 888 for x in [value1, value2]):
+            return NOT_ASSESSED_FLOAT
+
+        result = (value1 + value2) / 2  # type: ignore
+        # + 0.0001 so we ensure exact halves round up, not down
+        return round(result + 0.0001, 1)
+
+    def _create_naccwaist(self) -> Optional[float]:
+        """Creates NACCWAIST - Waist circumference (inches),
+        average of two measurements
+        """
+        return self._compute_average("waist1", "waist2")
+
+    def _create_nacchip(self) -> Optional[float]:
+        """Creates NACCHIP - Hip circumference (inches),
+        average of two measurements
+        """
+        return self._compute_average("hip1", "hip2")
+
+    def _handle_v3_blood_pressure(self, gate: str, field: str) -> float:
+        """Handles V3 blood pressure variables, which looks to see if
+        supplemental blood pressure information was provided and returns that
+        if so.
+
+        Args:
+            gate: Field indicating if supplement blood pressure data
+                was submitted (checking if it equals 777)
+            field: The field to grab if the supplemnt blood pressure
+                data was submitted
+
+        Returns:
+            value of field, if supplemental data provided, else -4
+        """
+        if self.uds.get_value(gate, int) == 777:
+            value = self.uds.get_value(field, float)
+            if value is None:
+                raise AttributeDeriverError(
+                    f"Missing expected value {field} when {gate} == 777 for V3"
+                )
+            return value
+
+        return float(INFORMED_MISSINGNESS)
+
+    def _create_naccbpsysl(self) -> Optional[float]:
+        """Creates NACCBPSYSL - Participant blood pressure
+        (average of two readings), systolic, left arm
+        """
+        if self.formver == 3:
+            return self._handle_v3_blood_pressure("bpsys", "bpsysl")
+
+        return self._compute_average("bpsysl1", "bpsysl2")
+
+    def _create_naccbpsysr(self) -> Optional[float]:
+        """Creates NACCBPSYSR - Participant blood pressure
+        (average of two readings), systolic, right arm
+        """
+        if self.formver == 3:
+            return self._handle_v3_blood_pressure("bpsys", "bpsysr")
+
+        return self._compute_average("bpsysr1", "bpsysr2")
+
+    def _create_naccbpdial(self) -> Optional[float]:
+        """Creates NACCBPDIAL - Participant blood pressure
+        (average of two readings), diastolic, left arm
+        """
+        if self.formver == 3:
+            return self._handle_v3_blood_pressure("bpdias", "bpdiasl")
+
+        return self._compute_average("bpdiasl1", "bpdiasl2")
+
+    def _create_naccbpdiar(self) -> Optional[float]:
+        """Creates NACCBPDIAR - Participant blood pressure
+        (average of two readings), diastolic, right arm
+        """
+        if self.formver == 3:
+            return self._handle_v3_blood_pressure("bpdias", "bpdiasr")
+
+        return self._compute_average("bpdiasr1", "bpdiasr2")
--- nacc_attribute_deriver/schema/constants.py
+++ nacc_attribute_deriver/schema/constants.py
@@ -1,6 +1,7 @@
 """Keeps track of global constants."""
 
 INVALID_TEXT = ["", ".", "`", "--", "-"]
+NOT_ASSESSED_FLOAT = 888.8
 INFORMED_MISSINGNESS = -4
 INFORMED_BLANK = "blank"  # will be translated to an overriding None
 
