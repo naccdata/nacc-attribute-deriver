a401fee85aafd41936a75a833252ca49c466f73c
Emily Cheng <echeng06@uw.edu>
2025-10-27 18:05:52 -0700

    RXCLASSES

--- nacc_attribute_deriver/schema/constants.py
+++ nacc_attribute_deriver/schema/constants.py
@@ -5,3 +5,38 @@ INFORMED_MISSINGNESS = -4
 INFORMED_BLANK = "blank"  # will be translated to an overriding None
 
 DERIVE_TYPES = ["create", "missingness"]
+
+# For form A4 derived variables
+ALL_RXCLASSES = [
+    "A10",
+    "B01",
+
+    "C01D",
+    "C02A",
+    "C02B",
+    "C02C",
+    "C02D",
+    "C02L",
+    "C03",
+    "C07A",
+    "C07B",
+    "C07C",
+    "C08",
+    "C09A",
+    "C09B",
+    "C09C",
+    "C09D",
+    "C10",
+
+    "G03C",
+    "G03F",
+
+    "N04",
+    "N05A",
+    "N05B",
+    "N05C",
+    "N06A",
+    "N06D",
+
+    "M01A",
+]
