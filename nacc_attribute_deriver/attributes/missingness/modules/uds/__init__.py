bd2d4ac8d5f370db29b06be090c3c9e43fd11c5d
Emily Cheng <echeng06@uw.edu>
2025-10-29 22:32:48 -0700

    omg it runs

--- nacc_attribute_deriver/attributes/missingness/modules/uds/__init__.py
+++ nacc_attribute_deriver/attributes/missingness/modules/uds/__init__.py
@@ -1,3 +1,14 @@
 # ruff: noqa
+from .missingness_a1 import *
+from .missingness_a1a import *
+from .missingness_a2 import *
+from .missingness_a3 import *
+from .missingness_a4 import *
+from .missingness_a4a import *
+from .missingness_b3 import *
 from .missingness_b5 import *
+from .missingness_b6 import *
+from .missingness_b8 import *
+from .missingness_b9 import *
+from .missingness_c1c2 import *
 from .missingness_uds import *
