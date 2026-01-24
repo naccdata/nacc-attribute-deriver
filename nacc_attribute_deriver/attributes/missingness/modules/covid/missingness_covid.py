0df4ec1a2c49ae10a85eb713e913a779eda24e70
Emily Cheng <echeng06@uw.edu>
2026-01-23 18:12:32 -0800

    never optional

--- nacc_attribute_deriver/attributes/missingness/modules/covid/missingness_covid.py
+++ nacc_attribute_deriver/attributes/missingness/modules/covid/missingness_covid.py
@@ -4,7 +4,7 @@ TODO: THIS IS ONLY REQUIRED TO BACKFILL WRITE-IN/KNOWN BLANK VARIABLES TO AVOID
 THE NOT-IN-CONTAINER ERROR. REMOVE ONCE FEATURE IS ADDED TO ETL GEAR.
 """
 
-from typing import Optional, Type
+from typing import Type
 
 from nacc_attribute_deriver.attributes.collection.missingness_collection import (
     FormMissingnessCollection,
@@ -19,7 +19,7 @@ class CovidFormMissingness(FormMissingnessCollection):
         variables."""
         return self.generic_missingness(field, attr_type)
 
-    def __fix_covid_year(self, field: str) -> Optional[int]:
+    def __fix_covid_year(self, field: str) -> int:
         """Fix improperly formatted covid years, e.g. 88 to 8888 and 99 to
         9999."""
         year = self.form.get_value(field, int)
@@ -33,14 +33,14 @@ class CovidFormMissingness(FormMissingnessCollection):
 
         return year
 
-    def _missingness_c19t1yr(self) -> Optional[int]:
+    def _missingness_c19t1yr(self) -> int:
         """Handles missingness for C19T1YR."""
         return self.__fix_covid_year("c19t1yr")
 
-    def _missingness_c19t2yr(self) -> Optional[int]:
+    def _missingness_c19t2yr(self) -> int:
         """Handles missingness for C19T2YR."""
         return self.__fix_covid_year("c19t2yr")
 
-    def _missingness_c19t3yr(self) -> Optional[int]:
+    def _missingness_c19t3yr(self) -> int:
         """Handles missingness for C19T3YR."""
         return self.__fix_covid_year("c19t3yr")
