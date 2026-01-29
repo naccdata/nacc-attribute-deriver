896d7abcf077bb7b3a34e096cecae0430e9cb0a0
Emily Cheng <echeng06@uw.edu>
2026-01-29 14:23:55 -0800

    update FTDLENGT

--- nacc_attribute_deriver/attributes/missingness/modules/ftld/missingness_ftld.py
+++ nacc_attribute_deriver/attributes/missingness/modules/ftld/missingness_ftld.py
@@ -7,6 +7,7 @@ from nacc_attribute_deriver.attributes.collection.missingness_collection import 
     SubjectMissingnessCollection,
 )
 from nacc_attribute_deriver.attributes.namespace.namespace import T
+from nacc_attribute_deriver.utils.constants import INFORMED_MISSINGNESS
 
 
 class FTLDMissingness(SubjectMissingnessCollection):
@@ -18,10 +19,6 @@ class FTLDMissingness(SubjectMissingnessCollection):
 
 
 class FTLDFormMissingness(FormMissingnessCollection):
-    """
-    TODO: THIS IS ONLY REQUIRED TO BACKFILL WRITE-IN/KNOWN BLANK VARIABLES TO AVOID
-    THE NOT-IN-CONTAINER ERROR. REMOVE ONCE FEATURE IS ADDED TO ETL GEAR.
-    """
 
     def _missingness_ftld(self, field: str, attr_type: Type[T]) -> T:
         """Defines general missingness for FTLD form variables."""
@@ -34,3 +31,17 @@ class FTLDFormMissingness(FormMissingnessCollection):
             return 9999
 
         return self.generic_missingness("ftdinfyr", int)
+
+    def _missingness_ftdlengt(self) -> int:
+        """Handles missingness for FTDLENGT."""
+        ftdlengt = self.form.get_value("ftdlengt", int) 
+
+        if ftdlengt is None:
+            return INFORMED_MISSINGNESS
+
+        # set 0 to 999
+        if ftdlengt in [0, 999]:
+            return 999
+
+        # otherwise, enforce range 20 - 240
+        return min(max(20, ftdlengt), 240)
