3ead7ac12210d139192d76f146a00afa3535821b
Emily Cheng <echeng06@uw.edu>
2026-07-27 13:39:35 -0700

    Add D1c, optional missingness

--- nacc_attribute_deriver/attributes/missingness/modules/uds/missingness_a1a.py
+++ nacc_attribute_deriver/attributes/missingness/modules/uds/missingness_a1a.py
@@ -5,6 +5,15 @@ from nacc_attribute_deriver.utils.constants import INFORMED_MISSINGNESS
 
 
 class UDSFormA1aMissingness(UDSMissingness):
+
+    def _missingness_langa1a(self) -> int:
+        """Handle missingness for LANGA1A."""
+        return self.handle_optional_header_variables("lang", "a1a")
+
+    def _missingness_admina1a(self) -> int:
+        """Handle missingness for ADMINA1A."""
+        return self.handle_optional_header_variables("admin", "a1a")
+
     def __handle_a1a_missingness(self, field: str) -> int:
         """Handle A1a missingness; basically need to set the default to 0 for
         all of these if V4."""
