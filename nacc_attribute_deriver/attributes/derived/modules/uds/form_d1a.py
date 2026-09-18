e73f31384dbd3709724bb6a2857358defdcbc3fa
Zach Stark <zstark@uw.edu>
2026-09-18 16:09:01 -0700

    This should return the value instead of collapsing the values into 1. v4 DED has valid 2, 3 values for NACCTBIDXIF

--- nacc_attribute_deriver/attributes/derived/modules/uds/form_d1a.py
+++ nacc_attribute_deriver/attributes/derived/modules/uds/form_d1a.py
@@ -173,7 +173,7 @@ class UDSFormD1aAttribute(UDSFormDxAttribute):
             tbidxif = self.uds.get_value("tbidxif", int)
             tbidx = self.uds.get_value("tbidx", int)
             if tbidxif in [1, 2, 3]:
-                return 1
+                return tbidxif
             if tbidx is None:
                 return 7
 
